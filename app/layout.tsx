57dcacdbd57d0434dd2110b0f25d0104fd3a6f2b
Zeeshan Nawaz <35934083+xee95@users.noreply.github.com>
2023-12-07 19:07:18 +0500

    companion form completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +19,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+    
+        <body className={cn("", inter.className)}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {/* <ProModal /> */}
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      
     </html>
+    </ClerkProvider>
   )
 }
